0878204714045107e5dfaa1a6ac42de2e33d72ed
Guilherme Henrique <guih@hotmail.com.br>
2015-02-16 03:27:34 -0200

    Main clean up

--- app.js
+++ app.js
@@ -1,29 +1,46 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+var express      = require('express');
+var path         = require('path');
+var favicon      = require('serve-favicon');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser   = require('body-parser');
+var session      = require('express-session');
+var captcha      = require('./lib/captcha');
+var Canvas       = require('canvas');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
+// routes
+var index = require('./routes/index');
+var data  = require('./routes/data');
+var auth  = require('./routes/auth');
+
+// libs
+var dataHunter = require('./lib/fetch');
+
+
+// available options 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'keyboard cat'
+}));
+
+
+app.use(/\/captcha\/([^\/]+)\/?/, captcha);
+app.use('/', index);
+app.get('/fetch/', data);
+app.post('/auth-session/', auth);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
